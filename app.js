8619dfca3fabe4412951acd3b2276d09614ee3ad
Kimberly Spiegel <k.spiegel72@gmail.com>
2019-07-13 16:12:57 -0400

    server running

--- app.js
+++ app.js
@@ -1,4 +1,14 @@
-function Pokemon(name,type,level,HP,attack,defense,exp){
+var express = require('express');
+var app = express();
+// var bodyParser = require('body-parser');
+var Pokedex = require('pokedex');
+var pokedex = new Pokedex();
+var port = process.env.PORT || 3000;
+
+// app.use(bodyParser.urlencoded({extended: true}));
+app.set('view engine', 'ejs');
+
+function Pokemon(name,type,level,HP,attack,defense,exp,animatedImage){
     this.name = name;
     this.type = type;
     this.level = level;
@@ -6,6 +16,7 @@ function Pokemon(name,type,level,HP,attack,defense,exp){
     this.attack = attack;
     this.defense = defense;
     this.exp = exp;
+    this.animatedImage = animatedImage;
     this.printStats = function(){
         console.log(name.toUpperCase()+"'S STATS");
         console.log('Pokemon: '+this.name+' Level: '+this.level);
@@ -28,18 +39,28 @@ function Pokemon(name,type,level,HP,attack,defense,exp){
 
 var pokemon = {};
 
-pokemon.bulbasaur = new Pokemon('Bulbasaur', 'Grass, Poison', 1, 45, 49, 49, 0);
-pokemon.charmander = new Pokemon('Charmander', 'Fire', 1, 39, 52, 43, 0);
-pokemon.squirtle = new Pokemon('Squirtle', 'Water', 1, 44, 48, 65, 0);
-pokemon.pikachu = new Pokemon('Pikachu', 'Electric', 1, 35, 55, 40, 0);
+pokemon.bulbasaur = new Pokemon('Bulbasaur', 'Grass, Poison', 1, 45, 49, 49, 0, pokedex.pokemon('bulbasaur').sprites.animated);
+pokemon.charmander = new Pokemon('Charmander', 'Fire', 1, 39, 52, 43, 0, pokedex.pokemon('charmander').sprites.animated);
+pokemon.squirtle = new Pokemon('Squirtle', 'Water', 1, 44, 48, 65, 0, pokedex.pokemon('squirtle').sprites.animated);
+pokemon.pikachu = new Pokemon('Pikachu', 'Electric', 1, 35, 55, 40, 0, pokedex.pokemon('pikachu').sprites.animated); 
 
-console.log(pokemon.bulbasaur.printStats());
+app.get('/',function(req,res){
+    res.render('landing', {pokemon: pokemon});
+});
 
-while (pokemon.bulbasaur.isConscious() && pokemon.charmander.isConscious()){
-    pokemon.bulbasaur.battle(pokemon.charmander);
-    pokemon.charmander.battle(pokemon.bulbasaur);
-    pokemon.bulbasaur.printStats();
-    console.log('------------------');
-    pokemon.charmander.printStats();
-    console.log('------------------');
-};
+
+
+// console.log(pokemon.bulbasaur.printStats());
+
+// while (pokemon.bulbasaur.isConscious() && pokemon.charmander.isConscious()){
+//     pokemon.bulbasaur.battle(pokemon.charmander);
+//     pokemon.charmander.battle(pokemon.bulbasaur);
+//     pokemon.bulbasaur.printStats();
+//     console.log('------------------');
+//     pokemon.charmander.printStats();
+//     console.log('------------------');
+// };
+
+app.listen(port, function(){
+    console.log('Pika pika!');
+});
