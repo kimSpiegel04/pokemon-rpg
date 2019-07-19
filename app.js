b50b015b05083d294b66ea6cbe2c06791eca1665
Kimberly Spiegel <k.spiegel72@gmail.com>
2019-07-19 18:05:52 -0400

    user and opponent show up

--- app.js
+++ app.js
@@ -2,64 +2,66 @@ var express = require('express');
 var app = express();
 // var bodyParser = require('body-parser');
 var Pokedex = require('pokedex');
+var nerds = require('nerds');
 var pokedex = new Pokedex();
+
+var computerChoice = nerds.resolve('Pokemon', 5).include(['name', 'type', 'hp', 'attack', 'defense', 'base_experience']).asArray();
+var userChoice = nerds.resolve('Pokemon', 5).include(['name', 'type', 'hp', 'attack', 'defense', 'base_experience']).asArray();
+
 var port = process.env.PORT || 3000;
 
 // app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static("public"));
 app.set('view engine', 'ejs');
 
-function Pokemon(name,type,level,HP,attack,defense,exp,animatedImage){
+////////////////USER POKEMON///////////////////////////////
+
+function Pokemon(name,type,level,HP,attack,defense,base_exp,animatedImage,stillImage){
     this.name = name;
     this.type = type;
     this.level = level;
     this.HP = HP;
     this.attack = attack;
     this.defense = defense;
-    this.exp = exp;
+    this.base_exp = base_exp;
     this.animatedImage = animatedImage;
+    this.stillImage = stillImage;
     this.printStats = function(){
-        console.log(name.toUpperCase()+"'S STATS");
-        console.log('Pokemon: '+this.name+' Level: '+this.level);
-        console.log('Type: '+this.type);
-        console.log('HP: '+this.HP);
-        console.log('Attack: '+this.attack+' Defense: '+this.defense);
-    };
-    this.isConscious = function(){
-        if(this.HP>0){
-            return true;
-        } else {
-            console.log(this.name+' passed out!');
-            return false;
-        }
-    };
-    this.battle = function(pokemon){
-        pokemon.HP -= parseInt(((2*this.level*10/250)*(this.attack/this.defense)*this.HP+2)*this.level);
-    };
+        var pokemonData = [
+            'Name: ' + this.name,
+            'Type: ' + this.type,
+            'Level: ' + this.level,
+            'HP: ' + this.HP + ' Base Exp.: ' + this.base_exp,
+            'Attack: ' + this.attack + ' Defense: ' + this.defense
+        ].join('\n\n');
+        console.log(pokemonData);
+    }
 }
 
-var pokemon = {};
+var userPokemon = [];
 
-pokemon.bulbasaur = new Pokemon('Bulbasaur', 'Grass, Poison', 1, 45, 49, 49, 0, pokedex.pokemon('bulbasaur').sprites.animated);
-pokemon.charmander = new Pokemon('Charmander', 'Fire', 1, 39, 52, 43, 0, pokedex.pokemon('charmander').sprites.animated);
-pokemon.squirtle = new Pokemon('Squirtle', 'Water', 1, 44, 48, 65, 0, pokedex.pokemon('squirtle').sprites.animated);
-pokemon.pikachu = new Pokemon('Pikachu', 'Electric', 1, 35, 55, 40, 0, pokedex.pokemon('pikachu').sprites.animated); 
+for(var i=0; i< userChoice.length; i++){
+    var newPokemon = new Pokemon (userChoice[i].name, userChoice[i].type, Math.floor(Math.random() * 10) + 1,  userChoice[i].hp, userChoice[i].attack, userChoice[i].defense, userChoice[i].base_experience,     pokedex.pokemon(userChoice[i].name.toLowerCase()).sprites.animated, pokedex.pokemon(userChoice[i].name.toLowerCase()).sprites.normal);
+
+    userPokemon.push(newPokemon);
+}
+
+////////////////COMPUTER POKEMON////////////////////////////
+
+var computerPokemon = [];
+
+for(var i=0; i< computerChoice.length; i++){
+    var newPokemon = new Pokemon (computerChoice[i].name, computerChoice[i].type, Math.floor(Math.random() * 10) + 1,  computerChoice[i].hp, computerChoice[i].attack, computerChoice[i].defense, computerChoice[i].base_experience, pokedex.pokemon(computerChoice[i].name.toLowerCase()).sprites.animated, pokedex.pokemon(computerChoice[i].name.toLowerCase()).sprites.normal);
+
+    computerPokemon.push(newPokemon);
+}
+
+
+console.log(userPokemon, computerPokemon);
 
 app.get('/',function(req,res){
-    res.render('landing', {pokemon: pokemon});
+    res.render('landing', {user:userPokemon,computer:computerPokemon});
 });
-
-
-
-// console.log(pokemon.bulbasaur.printStats());
-
-// while (pokemon.bulbasaur.isConscious() && pokemon.charmander.isConscious()){
-//     pokemon.bulbasaur.battle(pokemon.charmander);
-//     pokemon.charmander.battle(pokemon.bulbasaur);
-//     pokemon.bulbasaur.printStats();
-//     console.log('------------------');
-//     pokemon.charmander.printStats();
-//     console.log('------------------');
-// };
 
 app.listen(port, function(){
     console.log('Pika pika!');
